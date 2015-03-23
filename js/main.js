ac7f47c4103beaee8164ebdda9765b7d718b4f24
Paul-Myles <paulm@admins-iMac-2.local>
2015-03-24 12:13:56 +1300

    Samsung Micro Initial upload

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Landing text fade in
-$(".fade-h1").hide(0).delay(500).fadeIn(1200)
-$(".fade-h2").hide(0).delay(1300).fadeIn(2000)
-$("#bl-landing").delay(3000).fadeOut("slow")
-
-// Cursor fadeIn and animation
-/*$('.cursor').css('display', 'block');
-$('.cursor').animate({ opacity: 0 }, 0);
-$('.cursor').delay(2100).animate({ opacity: 1, right: "43%" }, 1900);*/
